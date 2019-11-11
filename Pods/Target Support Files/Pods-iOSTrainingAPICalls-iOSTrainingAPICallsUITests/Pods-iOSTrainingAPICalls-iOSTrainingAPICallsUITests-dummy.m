0dd8dc113e552b9ac4f962ab1482a148df81f7d9
Jun Marck Rael Legion <jun.legion@opsolutions.biz>
2019-11-11 16:38:05 +0800

    add gitignore file

--- Pods/Target Support Files/Pods-iOSTrainingAPICalls-iOSTrainingAPICallsUITests/Pods-iOSTrainingAPICalls-iOSTrainingAPICallsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSTrainingAPICalls_iOSTrainingAPICallsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_iOSTrainingAPICalls_iOSTrainingAPICallsUITests
-@end
--- Pods/Target Support Files/Pods-iOSTrainingAPICalls/Pods-iOSTrainingAPICalls-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSTrainingAPICalls : NSObject
-@end
-@implementation PodsDummy_Pods_iOSTrainingAPICalls
-@end
